46cdf80eacd8a372607037eb732c272a7e54da94
Tuan Anh <opensource1987@gmail.com>
2017-05-12 01:12:51 +0000

    update login with google+

--- plugins/google-auth/google_oauth_handler.js
+++ plugins/google-auth/google_oauth_handler.js
@@ -8,8 +8,8 @@ module.exports = function custom_handler(req, reply, tokens, profile) {
   //console.log(req);
   //console.log(reply);
   //console.log(tokens);
-  console.log(profile);
-  /*if(profile) {
+  //console.log(profile);
+  if(profile) {
     // extract the relevant data from Profile to store in JWT object
     var session = {
       firstname : profile.name.givenName, // the person's first name e.g: Anita
@@ -18,16 +18,23 @@ module.exports = function custom_handler(req, reply, tokens, profile) {
       exp      : Math.floor(new Date().getTime()/1000) + 7*24*60*60, // Epiry in seconds!
       agent    : req.headers['user-agent']
     };
+
     // create a JWT to set as the cookie:
     var token = JWT.sign(session, SECRET_KEY);
     // store the Profile and Oauth tokens in the Redis DB using G+ id as key
     // Detailed Example...? https://github.com/dwyl/hapi-auth-google/issues/2
 
-    // reply to client with a view
-    return reply("Hello " +profile.name.givenName + " You Logged in Using Goolge!")
-    .state('token', token); // see: http://hapijs.com/tutorials/cookies
-  }
-  else {
+    profile.tokens = tokens;
+    profile.valid = true;
+    
+    var redisClient = require('redis-connection')();
+    redisClient.set(session.id, JSON.stringify(profile), function (err, res) {
+
+      // reply to client with a view
+      return reply("Hello " +profile.name.givenName + " You Logged in Using Goolge!")
+      .state('token', token); // see: http://hapijs.com/tutorials/cookies
+    });
+  }else {
     return reply("Sorry, something went wrong, please try again.");
-  }*/
+  }
 };
